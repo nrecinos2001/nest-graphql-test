d68c588f1115b832a53d3ac79f27a0ce0f5dbbf4
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-01-10 10:26:27 -0600

    🔧 Add config module

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,5 +3,5 @@ export default () => ({
   database: {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-  }
-})
+  },
+});
--- src/config/env-validation-schema.ts
+++ src/config/env-validation-schema.ts
@@ -1,5 +1,5 @@
-import * as Joi from "joi";
+import * as Joi from 'joi';
 
 export default Joi.object({
-  PORT: Joi.number().required()
+  PORT: Joi.number().required(),
 });
