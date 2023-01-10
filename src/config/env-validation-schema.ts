5061188f6d2ed42efcb44e730919ca58840b4c71
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-01-10 10:33:08 -0600

    🔧 Add environment variables and their validations

--- src/config/env-validation-schema.ts
+++ src/config/env-validation-schema.ts
@@ -2,4 +2,10 @@ import * as Joi from 'joi';
 
 export default Joi.object({
   PORT: Joi.number().required(),
+  DB_TYPE: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(),
 });
