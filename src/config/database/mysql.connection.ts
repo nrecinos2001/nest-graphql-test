5560363398638f709bcf8d11c797d774e7db7152
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-01-12 12:08:54 -0600

    🔇 Remove logs

--- src/config/database/mysql.connection.ts
+++ src/config/database/mysql.connection.ts
@@ -1,6 +1,5 @@
 import { DataSource } from 'typeorm';
 
 import * as mySqlConnectionOptions from '../../../ormconfig.js';
-console.log(mySqlConnectionOptions.entities);
 
 export const mySqlConnection = new DataSource(mySqlConnectionOptions);
