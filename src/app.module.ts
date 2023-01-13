4abafb60cf55ccdf1445e32b2f8769e24bc8c262
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-01-13 11:25:26 -0600

    🔧 Update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorModule } from './author/author.module';
 import { AuthModule } from './auth/auth.module';
+import { ToDosModule } from './to-dos/to-dos.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     AuthorModule,
     AuthModule,
+    ToDosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
