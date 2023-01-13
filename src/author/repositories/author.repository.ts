882a3b991567711d0fbc5330d9dfaed99c96bdcc
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-01-13 11:17:56 -0600

    🗃️  Add relationships responses in author repository

--- src/author/repositories/author.repository.ts
+++ src/author/repositories/author.repository.ts
@@ -4,12 +4,15 @@ import { mySqlConnection } from 'src/config/database';
 
 export const AuthorRepository = mySqlConnection.getRepository(Author).extend({
   async findAll(): Promise<Author[]> {
-    const authors = await AuthorRepository.find();
+    const authors = await AuthorRepository.find({ relations: ['todos'] });
     return authors;
   },
 
-  async findOneById(id: number): Promise<Author> {
-    const author = await AuthorRepository.findOne({ where: { id } });
+  async findOneByAuthorId(id: number): Promise<Author> {
+    const author = await AuthorRepository.findOne({
+      where: { id },
+      relations: ['todos'],
+    });
     return author;
   },
 
