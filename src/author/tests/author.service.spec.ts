7de72a02a8f0428cdf44b24ee38198ae1446e90b
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-01-19 11:38:22 -0600

    ✅ Add Author tests

--- src/author/tests/author.service.spec.ts
+++ src/author/tests/author.service.spec.ts
@@ -1,8 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthorService } from '../services/author.service';
+import { AuthorRepository } from '../repositories';
+import {
+  author,
+  authorOutput,
+  createAuthorInput,
+} from 'src/author/tests/mocks';
 
 describe('AuthorService', () => {
   let service: AuthorService;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthorService],
@@ -13,5 +20,19 @@ describe('AuthorService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('createAuthor', () => {
+    describe('When a new Author is created', () => {
+      it('should return the created author', async () => {
+        jest
+          .spyOn(AuthorRepository, 'createOne')
+          .mockImplementation(async () => {
+            return author;
+          });
+        const result = service.create(createAuthorInput);
+        await expect(result).resolves.toEqual(authorOutput);
+      });
+    });
   });
 });
