e65aac13f7f354f9a62b4fefe3fe8fd8dafecd37
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-01-19 12:19:22 -0600

    🤡 Mock authors

--- src/author/tests/mocks/author.data.mock.ts
+++ src/author/tests/mocks/author.data.mock.ts
@@ -1,11 +1,20 @@
 import { AuthorOutput, CreateAuthorInput } from 'src/author/dto';
 import { Author } from 'src/author/entities';
 
-export const author: Author = {
+export const johnDoe: Author = {
   id: 1,
   name: 'John Doe',
   email: 'john@doe.com',
   username: 'johnDoe',
+  password: 'pasword',
+  todos: [],
+};
+
+export const kamilDoe: Author = {
+  id: 1,
+  name: 'Kamil Doe',
+  email: 'kamil@doe.com',
+  username: 'kamilDoe',
   password: 'pasword',
   todos: [],
 };
@@ -17,10 +26,22 @@ export const createAuthorInput: CreateAuthorInput = {
   password: 'password',
 };
 
-export const authorOutput: AuthorOutput = {
+export const johnDoeOutput: AuthorOutput = {
   id: 1,
   name: 'John Doe',
   email: 'john@doe.com',
   username: 'johnDoe',
   todos: [],
 };
+
+export const kamilDoeOutput: AuthorOutput = {
+  id: 1,
+  name: 'Kamil Doe',
+  email: 'kamil@doe.com',
+  username: 'kamilDoe',
+  todos: [],
+};
+
+export const authors: Author[] = [johnDoe, kamilDoe];
+
+export const authorsOutput: AuthorOutput[] = [johnDoeOutput, kamilDoeOutput];
